8f81c54e60e7be86494e2a5cbfe646a71e3a8648
多厘 <zhaody901@126.com>
2022-05-23 12:14:39 +0800

    init

---
+++ src/components/Scoped.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+</script>
+
+<template>
+	<div class="step">{{msg}}</div>
+</template>
+
+<style scoped>
+.step {
+	padding-top: 20px;
+}
+</style>
